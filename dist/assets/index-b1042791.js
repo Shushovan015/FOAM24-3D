3c32d876909f272bf04b45c202d099d60bc46eaf
Shushovan shakya <shushovan015@gmail.com>
2025-01-24 12:55:38 +0100

    implementd image outline api using docker

--- dist/assets/index-b1042791.js
+++ dist/assets/index-b1042791.js
@@ -53343,7 +53343,7 @@ const createShapePhotoShape = (millimeters2, selected2, shapesArray2, commit2, s
       }).then((blob) => {
         const formData = new FormData();
         formData.append("image", blob, "image.png");
-        return fetch("http://127.0.0.1:5000/detect_contours", {
+        return fetch("http://fm24api.com/detect_contours", {
           method: "POST",
           body: formData
         });
@@ -54357,4 +54357,4 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 });
-//# sourceMappingURL=index-5db9d3de.js.map
+//# sourceMappingURL=index-b1042791.js.map
